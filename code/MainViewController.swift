60e451fb70770acdf15e31ed691bf11a8af28aca
Brandon Siebert <SiebertBrandon@users.noreply.github.com>
2017-12-05 00:58:59 -0500

    Added Header

--- code/MainViewController.swift
+++ code/MainViewController.swift
@@ -1,9 +1,12 @@
-//
-//  MainViewController.swift
-//  Discuss Action
-//
-//  Copyright © 2017 Brandon Siebert. All rights reserved.
-//
+// Name: Brandon Siebert
+// Course: CSC 415
+// Semester: Fall 2017
+// Instructor: Dr. Pulimood 
+// Project name: Discuss Action
+// Description: iOS application that facilitates active discussion for social justice issues. 
+// Filename: MainViewController.swift
+// Description: Entry point for the application. Controls the main event view list.
+// Last modified on: 12/4/2017
 
 import UIKit
 
